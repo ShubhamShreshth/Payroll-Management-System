fdd21336281b706cd0dfd2db89eda8c85e35b894
Shubham Shreshth <50519329+ShubhamShreshth@users.noreply.github.com>
2021-09-15 16:38:54 +0530

    Update payroll.sql

--- sql/payroll.sql
+++ sql/payroll.sql
@@ -39,8 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, '1604071', '12345'),
-(2, '1604070', 'montu');
+(1, '100', '12345');
 
 -- --------------------------------------------------------
 
@@ -58,11 +57,11 @@ CREATE TABLE `department` (
 --
 
 INSERT INTO `department` (`Depart_id`, `Depart_name`) VALUES
-(104, 'hardwere'),
-(105, 'softwere'),
-(106, 'design'),
-(107, 'accessories'),
-(108, 'codsection');
+(104, 'Hardware'),
+(105, 'Software'),
+(106, 'Design'),
+(107, 'Accessories'),
+(108, 'Code-Section');
 
 -- --------------------------------------------------------
 
@@ -93,16 +92,7 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`Employee_id`, `Name`, `Address`, `Phone_no`, `Email`, `Start_date`, `d_birth`, `gender`, `loan`, `pfund`, `salary`, `jobtitle`, `Depart_id`, `managesDepart_id`, `b_accno`) VALUES
-(1604023, 'Rukon', 'Sojahnpur, bogra', '01521420202', 'rukonali@gmail.com', '2018-10-10', '1996-02-29', 'male', 0, 2000, 40000, 'manager', 108, 106, 236954128),
-(1604025, 'Raufur Rahman', 'Jamalganj', '01767884314', 'shimanto@gmail.com', '2018-12-18', '2000-01-01', 'male', 3610, 1500, 35000, 'executive', 105, 104, 123654784),
-(1604026, 'Sibbir ahmed', 'mirpur ,dhaka', '01684959574', 'sibbir026@gmail.com', '2018-08-14', '1996-07-11', 'male', 0, 1250, 50000, 'manager', 106, 107, 3654889),
-(1604027, 'nahid riaz swachha', 'dinajpur, rangpur', '01797635121', 'nahidriaz284@gmail.com', '2018-11-01', '1998-09-11', 'male', 10000, 750, 35000, 'executive', 107, 108, 31351566),
-(1604045, 'Md..Faridul Islam', 'Shohagpur,Mukundoganti,Belkuchi,Sirajganj', '01521430613', 'dhcfaridul@gmail.com', '2019-01-18', '1997-10-25', 'male', 4512.5, 750, 35000, 'executive', 105, 105, 125432874),
-(1604060, 'Rakibul Islam', 'Shippur , Narsingdi', '01781932649', 'rakibcuet16@gmail.com', '2019-01-03', '1997-06-04', 'male', 0, 1250, 40000, 'manager', 106, 106, 15497830),
-(1604073, 'Nahidul islam apu', 'town hall, mymensingh', '01986980335', 'nahidul@gmail.com', '2018-09-11', '1997-01-25', 'male', 0, 1500, 60000, 'accountant', 105, 108, 2147483647),
-(1604078, 'Mehedi Hasan', 'Nandigram,Bogra', '01521406329', 'hasanmahadi877@gmail.com', '2019-01-18', '1998-03-02', 'male', 0, 1750, 70000, 'chief', 106, 108, 2147483647),
-(1604083, 'Mahathir bishal', 'dinajpur, rangpur', '01747424468', 'mahathirbishal@gmail.com', '2014-06-02', '1997-11-14', 'male', 0, 750, 35000, 'executive', 106, 106, 321569874),
-(1604110, 'Rushnan rifa', 'munshuganj', '01763710093', 'rushnan@gmail.com', '2018-10-22', '1999-08-28', 'female', 0, 1250, 50000, 'director', 108, 106, 2147483647);
+(101, 'Shubham', 'Bangalore', '01521420202', 'shubham@gmail.com', '2018-10-10', '1996-02-29', 'male', 0, 2000, 40000, 'manager', 108, 106, 236954128);
 
 -- --------------------------------------------------------
 
@@ -121,7 +111,7 @@ CREATE TABLE `loginform` (
 --
 
 INSERT INTO `loginform` (`id`, `user`, `pass`) VALUES
-(1, '1604071', 'password');
+(1, '101', 'password');
 
 -- --------------------------------------------------------
 
@@ -152,8 +142,7 @@ CREATE TABLE `payment` (
 --
 
 INSERT INTO `payment` (`pay_no`, `emp_id`, `name`, `accno`, `year`, `month`, `absence`, `loan_cut`, `pfund_cut`, `overtime`, `season_bonus`, `other_bonus`, `medi_allow`, `house_allow`, `total_pay`) VALUES
-(1, '160403', '', 0, 2018, 'december', 2, 0, 0, 6, 2000, 0, 0, 0, 3400),
-(2, '1604023', 'Rukon', 236954128, 2018, 'december', 2, 0, 1000, 6, 2000, 0, 1200, 3200, 46800);
+(1, '101', 'Shubham', 236954128, 2018, 'december', 2, 0, 1000, 6, 2000, 0, 1200, 3200, 46800);
 
 --
 -- Indexes for dumped tables
